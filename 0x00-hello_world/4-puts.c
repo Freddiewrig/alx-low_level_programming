8580adcaee4b6b1d4e7d2a8729f0e524a0a1af75
Freddiewrig <thecollegeace1@gmail.com>
2023-07-13 10:33:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/* 
+ * This program prints the string "programming is like building a multiligual puzzle"
+ * to an output followed by a new line. the program ends with the value 0.
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
