64f3d2a44ef15f0964960e20ec4b358cf24e841a
Freddiewrig <thecollegeace1@gmail.com>
2023-07-17 08:06:43 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,22 +7,25 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 8; i++)
+	for (i = 48; i < 56; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j < 57; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k < 58; k++)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-				if (!(i == 7 && j == 8 && k == 9))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+					continue;
+
+					putchar(44);
+					putchar(32);
 			}
 		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
