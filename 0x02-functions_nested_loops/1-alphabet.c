099cd3a7c501b203d30bfa0d8a5ff80f7a00f986
Freddiewrig <thecollegeace1@gmail.com>
2023-07-18 13:13:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * main - Entry point
+ * prints alphabets in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c
+
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	_putchar('\n');
+	return (0);
+}
