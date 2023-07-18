ac85fa085b3413156ec997910c65427fd708f331
Freddiewrig <thecollegeace1@gmail.com>
2023-07-18 15:39:54 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit number
+ * @n: number checked
+ * Return: value of n
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
+}
