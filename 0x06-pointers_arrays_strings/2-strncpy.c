cd46c8688adddc306cc2c3ee9800fac6bf2f24e7
Freddiewrig <thecollegeace1@gmail.com>
2023-07-26 21:44:41 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,8 +10,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0' && j < n; j++)
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
 		dest[j] = src[j];
+		j++;
+	}
 	while (j < n)
 	{
 		dest[j] = '\0';
