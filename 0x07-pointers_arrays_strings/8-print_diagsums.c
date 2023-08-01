c0b62accefb6ba286b579399f4df9bc21627e3e0
Freddiewrig <thecollegeace1@gmail.com>
2023-08-01 09:07:57 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: Double pointer to char to be updated
+ * @to: Pointer to char whose value will be assigned to @s
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
