155bc91582a8e88dd194d4f07cc6af207d4a3a84
Freddiewrig <thecollegeace1@gmail.com>
2023-08-03 02:31:33 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,28 @@
 #include "main.h"
 /**
- * is_prime_number - returns 1 if the input integer is a
+ * is_prime_number_new - helper
  * prime number, otherwise return 0
- * @n: input
+ * @n: number checked
+ * @i: input
  * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number_new(int n, int i)
+{
+	if (n < 2)
+		return (0);
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (n || i + 1);
+}
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ * @n: number checked
+ * Return: prime number
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n <= 3)
-	{
-		return (1);
-	}
-	else if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		for (i = 5; i * i <= n; i += 6)
-		{
-			if (n % i == 0 || n % (i + 2) == 0)
-			{
-				return (0);
-			}
-		}
-		return (1);
-	}
+	return (is_prime_number_new(n, 2));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt_recursion_new(int n, int low, int high);
+int is_prime_number_new(int n, int i);
 #endif
