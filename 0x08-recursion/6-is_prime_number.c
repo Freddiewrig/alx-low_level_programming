827de7f3893bbb19b3146601b9734fba8cf99bcf
Freddiewrig <thecollegeace1@gmail.com>
2023-08-03 01:56:09 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,26 @@ int is_prime_number(int n)
 	int i;
 
 	if (n <= 1)
+	{
 		return (0);
-	for (i = 0; i < n; i++)
-		if (n % i == 0)
-			return (0);
-	return (1);
+	}
+	else if (n <= 3)
+	{
+		return (1);
+	}
+	else if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 5; i * i <= n; i += 6)
+		{
+			if (n % i == 0 || n % (i + 2) == 0)
+			{
+				return (0);
+			}
+		}
+		return (1);
+	}
 }
