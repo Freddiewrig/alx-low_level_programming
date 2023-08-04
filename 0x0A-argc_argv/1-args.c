3be2e55904187bb5dfefaf3aceaa750e3325a06d
Freddiewrig <thecollegeace1@gmail.com>
2023-08-04 09:52:39 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints number of urguments
+ * @argc: prints number of urguments
+ * @argv: urguments passed
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
