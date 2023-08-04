7f65df1cab7ccf2cd4e23b82d7685acd9ed63028
Freddiewrig <thecollegeace1@gmail.com>
2023-08-04 09:32:36 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints programs name
  * @argc: number of urguments passed
  * @argv: the urgument passed
  * Return: 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void)argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
