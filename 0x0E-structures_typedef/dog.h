df70529da12958607d252ac8dbe712c067a74558
Freddiewrig <thecollegeace1@gmail.com>
2023-08-14 07:35:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog infor
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
