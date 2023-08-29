f86379cc5f7f5ea806087ff5a112cbedb7eae3b5
Freddiewrig <thecollegeace1@gmail.com>
2023-08-29 07:11:12 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 /**
  * struct listint_s - Structure for a singly linked list node
  * @n: Integer value stored in the node
@@ -11,4 +12,5 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 #endif /* LISTS_H */
